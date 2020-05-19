1721783cbdd1663eb05bc4c550b334532f58853a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-18 21:32:10 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+#new_doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+#puts doc
+puts doc.css(".headline-260IBN").text
